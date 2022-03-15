dd5c49b5ea5bb5339ab13aeb176b82c77533ed04
hanhan0-0 <2970483741@qq.com>
2022-03-15 15:58:17 +0800

    jsonp跨域

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div></div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import jsonp from 'jsonp'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  
+  },
+  data(){
+    return{
+      age:30,
+      data:''
+    }
+  },
+  mounted(){
+    let url="https://www.imooc.com/activity/servicetime";
+    jsonp(url,(eer,res)=>{
+      let result=res;
+      this.data=result;
+    })
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
