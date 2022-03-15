4ca94bd59c2b211ed5ddb1e256894f7565cf6dd3
hanhan0-0 <2970483741@qq.com>
2022-03-15 16:03:00 +0800

    cors跨域学习

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div >
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   
+  },
+  mounted(){
+    let url=""
+    axios.get(url).then(()=>{
+
+    })
   }
 }
 </script>
