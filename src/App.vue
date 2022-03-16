c87bcba91407c085b5f101f4e56288484651f06e
hanhan0-0 <2970483741@qq.com>
2022-03-16 20:59:41 +0800

    接口代理

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,26 @@
 </template>
 
 <script>
-
+import jsonp from "jsonp"
 
 export default {
   name: 'App',
+  data(){
+    return{
+      age:30,
+      data:''
+    }
+  },
   components: {
     
+  },
+  mounted(){
+    let url="/api/activity/servicetime";
+    jsonp(url,(err,res)=>{
+      
+      let result=res;
+      this.data=result;
+    })
   }
 }
 </script>
