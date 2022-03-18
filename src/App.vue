7dcc90bff7cfc1f0b62d52978206bc82e1da8cc7
hanhan0-0 <2970483741@qq.com>
2022-03-18 16:38:38 +0800

    路由封装

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div >
-    
+    <router-view> </router-view>
   </div>
 </template>
 
 <script>
-
+// import jsonp from "jsonp"
 
 export default {
   name: 'App',
+  data(){
+    return{
+     
+    }
+  },
   components: {
     
+  },
+  mounted(){
+       
   }
 }
 </script>
