c5a5feeaf049d48ef07aebdd054baab67fd0134e
hanhan0-0 <2970483741@qq.com>
2022-04-11 17:07:35 +0800

    cartend

--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -8,20 +8,31 @@
                 <h2>{{title}}<slot name="tip"></slot></h2>
             </div>
             <div class="username">
-                <a href="javascript:;">Jack</a>
+                <a href="javascript:;">{{username}}</a>
             </div>
         </div>
     </div>
 </template>
 <script>
+// import {mapState} from 'vuex'
+import storage from '../storage'
+
 export default {
     name:'order-header',
     props:{
         title:String,
+    },
+    computed:{
+        // ...mapState(['username','cartCount'])
+            username(){
+                return storage.getItem("user");
+            }
+        
     }
 }
 </script>
 <style lang="scss">
+ 
     .order-header{
         padding: 30px 0px;
         .header-logo{
